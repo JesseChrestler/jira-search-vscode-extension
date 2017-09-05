3fc29735ebb50e153a22beea442dc5d7deab0548
Jesse Chrestler <jessec@xceleration.com>
2017-09-05 18:31:15 -0400

    tree-provider init

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,15 @@
 'use strict';
-import * as vscode from "vscode";
-import * as jira from "./jira";
-
+import * as vscode from 'vscode';
+import * as jira from './jira';
+import { JiraTreeProvider } from './jiraTreeProvider';
 export function activate(context: vscode.ExtensionContext) {
-    vscode.workspace.onDidChangeConfiguration(jira.updateConfiguration);
-    let disposable = vscode.commands.registerCommand('extension.jiraSearch', () => {
-        jira.updateConfiguration().then(() => jira.showIssues());
-    });
-    context.subscriptions.push(disposable);
+  vscode.workspace.onDidChangeConfiguration(jira.updateConfiguration);
+  const nodeDependenciesProvider = new JiraTreeProvider();
+  let disposable = vscode.commands.registerCommand('extension.jiraSearch', () => {
+    jira.updateConfiguration().then(() => jira.showIssues());
+  });
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
